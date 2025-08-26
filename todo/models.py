1a22aa95896d4ddd5fe50c26fdc124d7e7804c15
Prince Kumar Singh <princekumarsingh167@gmail.com>
2025-08-26 11:32:39 +0530

    Initiol commit

---
+++ todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+     
+    
